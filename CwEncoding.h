426064a6d1ba90bc7d664bb3ff8eea41b19a58a3
Mario Fischer <ich@mariofischer.name>
2012-09-07 22:47:15 +0200

    namespacing.

---
+++ CwEncoding.h
@@ -0,0 +1,28 @@
+//
+//  Encoding.h
+//  PhpPlugin
+//
+//  Created by Mario Fischer on 06.11.09.
+//  Copyright 2009 chipwreck.de. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CwEncoding : NSObject <NSCoding>
+{
+@public
+    int intvalue;
+    NSString *title;
+	NSString *cmdLineParam;
+}
+
+@property int intvalue;
+@property (copy) NSString *title;
+@property (copy) NSString *cmdLineParam;
+
++ (NSArray *)encodingsArray;
++ (id)encodingWithTitle:(NSString *)aTitle intvalue:(int)aValue cmdLine:(NSString *)aCmdLine;
++ (CwEncoding *)encodingForIndex:(int)theIdx;
++ (CwEncoding *)encodingForIntvalue:(int)theValue;
+
+@end
